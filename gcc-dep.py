052f022613bccff93a0ed0eaa4bbde0fb2436412
Timothy Madden <terminatorul@gmail.com>
2019-11-19 05:11:01 +0200

    Fix build error for missing headers with 'gcc-dep'
    Fix explicit dependency error message from SCons, triggerred with
    `gcc-dep` tool, when a previous hearder file is now missing.
    
    Add XRefParseDepends() function base on SCons ParseDepends() and
    logical_lines() functions, that can:
        - parse file names with spaces
        - provide option to add only dependencies that exist in the
          file system
    
    New function is now enabled by default instead of SCons
    env.ParseDepends() function..

--- gcc-dep.py
+++ gcc-dep.py
@@ -26,6 +26,97 @@ from SCons.Action  import ListAction
 
 import source_browse_base as base
 
+def logical_lines(physical_lines, joiner = ''.join):
+    logical_line = [ ]
+
+    for line in physical_lines:
+        stripped = line.rstrip()
+        if stripped.endswith('\\'):
+            # a line which continues w/the next physical line
+            logical_line.append(stripped[:-1])
+        else:
+            # a line which does not continue, end of logical line
+            logical_line.append(line)
+            yield joiner(logical_line)
+            logical_line = [ ]
+
+    if logical_line:
+        # end of sequence implies end of last logical line
+        yield joiner(logical_line)
+
+def XRefParseDepends(self, filename, must_exist = None, only_one = 0, existing_only = False):
+    """
+        Similar to SCons environment ParseDepends() method, with the following additions:
+            - filenames with spaces and tabs are properly parsed, as long as file names do not end with
+              the escape character '\\', which can trigger errors.
+            - a target filename with colons is properly parsed
+            - can add only explicit dependencies that exist in the file system, so if user deletes or
+              moves a header file, the build can still proceed as usual
+    """
+    filename = self.subst(filename)
+
+    try:
+        with open(filename, 'r') as fp:
+            lines = [ line for line in logical_lines(fp, '\\\n'.join) ]
+    except IOError:
+        if must_exist:
+            raise
+        return
+
+    lines = [ l for l in lines if l[0] != '#' ]
+
+    tdlist = [ ]
+
+    for line in lines:
+        try:
+            target, depends = re.split(':(?:(?:\s+)|(?:\\\\\n\s+)|$)+', line, 1)
+        except (AttributeError, ValueError):
+            # Throws AttributeError if line isn't a string.  Can throw
+            # ValueError if line doesn't split into two or more elements.
+            pass
+        else:
+            target_list = re.split('(?:(?:(?<!\\\\)\s)|(?:\s\\\\\n\s*)|(?:\\\\\n\s+))+', target)
+            source_list = re.split('(?:(?:(?<!\\\\)\s)|(?:\s\\\\\n\s*)|(?:\\\\\n\s+))+', depends)
+
+            targets = [ ]
+            sources = set()
+
+            unescape = lambda match: '\\' * ((len(match.group(0)) - 2)/2) + match.group(0)[-1]
+
+            for tgt in target_list:
+                if tgt:
+                    tgt = re.sub('\\\\\n', '', tgt)
+                    targets.append(re.sub('\\\\*\\\\\\s', unescape, tgt))
+
+            build_dir = self.Dir('#').abspath
+
+            for src in source_list:
+                if src:
+                    src = re.sub('\\\\\n', '', src)
+                    src = re.sub('\\\\*\\\\\\s', unescape, src)
+
+                    if not existing_only or (os.path.isfile(src) if os.path.isabs(src) \
+                            else os.path.isfile(os.path.join(build_dir, src))):
+                        sources.add(src)
+
+            tdlist.append((targets, list(sources)))
+
+    if only_one:
+        targets = [ ]
+
+        for td in tdlist:
+            targets.extend(td[0])
+
+        if len(targets) > 1:
+            raise SCons.Errors.UserError\
+                    (
+                        "More than one dependency target found in `%s':  %s" % (filename, targets)
+                    )
+
+    else:
+        for target, depends in tdlist:
+            self.Depends(target, depends)
+
 def gcc_dep_emitter(target, source, env):
     """
         emitter function for SCons Builders, injected into the existing Object / SharedObject
@@ -52,7 +143,8 @@ def gcc_dep_emitter(target, source, env):
             env.fs.chdir(env.Dir('#'))
 
             try:
-                env.ParseDepends(dep_file.get_abspath())
+                # env.ParseDepends(dep_file.get_abspath())
+                env.XRefParseDepends(dep_file.get_abspath(), existing_only = True)
             finally:
                 env.fs.chdir(script_dir)
 
@@ -61,7 +153,7 @@ def gcc_dep_emitter(target, source, env):
     return target, source
 
 def reload_dependency_file(target, source, env):
-    env.ParseDepends(env.subst('$GCCDEP_FILENAME', False, target, source))
+    env.XRefParseDepends(env.subst('$GCCDEP_FILENAME', False, target, source))
 
 def generate(env, **kw):
     """
@@ -145,6 +237,7 @@ def generate(env, **kw):
                     pass
 
         env['GCCDEP_INJECTED'] = True
+        env.AddMethod(XRefParseDepends)
 
 def exists(env):
     """ Return True if the tool is present in the environment """
